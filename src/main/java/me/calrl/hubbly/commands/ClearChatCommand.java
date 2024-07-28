b8d8e43d6130a8ec47466ab46c497fd015df1016
Cal <43823915+CalRL@users.noreply.github.com>
2024-07-28 18:11:21 +0100

    Updated 3 files
    ClearChat now runs on the bukkit scheduler runTask() method
    Added the GPL licensing to all 3 files

--- src/main/java/me/calrl/hubbly/commands/ClearChatCommand.java
+++ src/main/java/me/calrl/hubbly/commands/ClearChatCommand.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Hubbly.
+ *
+ * Hubbly is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Hubbly is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hubbly. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package me.calrl.hubbly.commands;
 
 import me.calrl.hubbly.Hubbly;
@@ -14,11 +31,13 @@ public class ClearChatCommand implements SubCommand {
     @Override
     public void execute(Player player, String[] args) {
         if(player.hasPermission("hubbly.clearchat") || player.isOp()) {
-            int i = 0;
-            while (i <= 50) {
-                Bukkit.broadcastMessage("");
-                i++;
-            }
+            Bukkit.getScheduler().runTask(plugin, () -> {
+                int i = 0;
+                while (i <= 50) {
+                    Bukkit.broadcastMessage("");
+                    i++;
+                }
+            });
             plugin.getDebugMode().info("Chat cleared by " + player.getName());
         }
 
