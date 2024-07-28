7864ee05f224519c4702c3836b474e05bcdf9cd2
Cal <43823915+CalRL@users.noreply.github.com>
2024-07-28 17:46:55 +0100

    Updated ChatListener.java
    final version of this probably unless i think of something to change
    
    added repeat function to ChatUtils.java
    starting to make announcements with intervals

--- src/main/java/me/calrl/hubbly/listeners/chat/ChatListener.java
+++ src/main/java/me/calrl/hubbly/listeners/chat/ChatListener.java
@@ -1,6 +1,8 @@
 package me.calrl.hubbly.listeners.chat;
 
 import me.calrl.hubbly.Hubbly;
+import me.calrl.hubbly.managers.DebugMode;
+import me.calrl.hubbly.utils.ChatUtils;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,32 +11,42 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import java.util.List;
 import java.util.Objects;
 
-import static jdk.internal.joptsimple.internal.Strings.repeat;
 
 public class ChatListener implements Listener {
     private final Hubbly plugin;
-    private List<String> blockedWords;
+    private final List<String> blockedWords;
+    private final DebugMode debugMode;
     public ChatListener(Hubbly plugin) {
         this.plugin = plugin;
         blockedWords = plugin.getConfig().getStringList("blocked_words.words");
+        debugMode = plugin.getDebugMode();
     }
 
     @EventHandler
     private void onPlayerChat(AsyncPlayerChatEvent event) {
         FileConfiguration config = plugin.getConfig();
-        if(plugin.getDisabledWorldsManager().inDisabledWorld(event.getPlayer().getWorld())) return;
         if(!config.getBoolean("blocked_words.enabled")) return;
+        if(plugin.getDisabledWorldsManager().inDisabledWorld(event.getPlayer().getWorld())) return;
+
 
         String message = event.getMessage();
         String method = config.getString("blocked_words.method");
-        
+
         for(String word : blockedWords) {
+            if(method == null) {
+                debugMode.info("blocked_words.method is null in config");
+                return;
+            }
+
             if(Objects.equals(method.toUpperCase(), "CANCEL")) {
+
                 event.setCancelled(true);
                 event.getPlayer().sendMessage(Objects.requireNonNull(config.getString("messages.blocked_message")));
                 return;
+
             } else if (Objects.equals(method.toUpperCase(), "STAR")) {
-                message = message.replace(word, repeat('*', word.length()));
+
+                message = message.replace(word, ChatUtils.repeat("*", word.length()));
                 event.setMessage(message);
             }
         }
--- src/main/java/me/calrl/hubbly/utils/ChatUtils.java
+++ src/main/java/me/calrl/hubbly/utils/ChatUtils.java
@@ -54,4 +54,9 @@ public class ChatUtils {
 
         return message;
     }
+
+    public static String repeat(String string, int count) {
+        return new String(new char[count]).replace("\0", string);
+    }
+
 }
