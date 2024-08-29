d10f0bf267e89c22e7706f541bf1cc02e5eaa141
Cal <43823915+CalRL@users.noreply.github.com>
2024-08-29 01:46:34 +0100

    Added SOCIALS and SERVER SELECTOR
     to pluginkeys for nbt data

--- src/main/java/me/calrl/hubbly/enums/PluginKeys.java
+++ src/main/java/me/calrl/hubbly/enums/PluginKeys.java
@@ -3,8 +3,14 @@ package me.calrl.hubbly.enums;
 import me.calrl.hubbly.Hubbly;
 import org.bukkit.NamespacedKey;
 
+import javax.naming.Name;
+
 public enum PluginKeys {
-    PLAYER_VISIBILITY(new NamespacedKey(Hubbly.getInstance(), "player_visibility"));
+
+    PLAYER_VISIBILITY(new NamespacedKey(Hubbly.getInstance(), "player_visibility")),
+    SOCIALS(new NamespacedKey(Hubbly.getInstance(), "socials")),
+    SELECTOR(new NamespacedKey(Hubbly.getInstance(), "selector"));
+
 
     private final NamespacedKey key;
     PluginKeys(NamespacedKey key) {
