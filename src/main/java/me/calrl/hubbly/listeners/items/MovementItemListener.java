f0724cfc370c6ea902edf0d02b42e846052ea1b1
Cal <callumburnsoregan@gmail.com>
2024-09-01 20:51:13 +0100

    Added grappling_hook logic

--- src/main/java/me/calrl/hubbly/listeners/items/MovementItemListener.java
+++ src/main/java/me/calrl/hubbly/listeners/items/MovementItemListener.java
@@ -4,6 +4,7 @@ import me.calrl.hubbly.Hubbly;
 import me.calrl.hubbly.enums.PluginKeys;
 import me.calrl.hubbly.enums.TridentSounds;
 import me.calrl.hubbly.items.TridentItem;
+import me.calrl.hubbly.managers.cooldown.CooldownType;
 import net.minecraft.world.entity.projectile.EntityThrownTrident;
 import net.minecraft.world.item.ItemFishingRod;
 import org.bukkit.GameMode;
@@ -22,6 +23,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.util.Vector;
 
 import java.util.Random;
 
@@ -97,14 +99,27 @@ public class MovementItemListener implements Listener {
 
     @EventHandler
     private void onFishingRodUse(PlayerFishEvent event) {
-        if(!config.getBoolean("movementitems.fishing_rod.enabled")) return;
+        if (!config.getBoolean("movementitems.grappling_hook.enabled")) return;
+
         Player player = event.getPlayer();
-        ItemMeta meta = player.getInventory().getItemInMainHand().getItemMeta();
-        if(event.getState() != PlayerFishEvent.State.FAILED_ATTEMPT || event.getState() != PlayerFishEvent.State.IN_GROUND) return;
-        if(meta != null && meta.getPersistentDataContainer().has(PluginKeys.FISHING_ROD.getKey())){
-            if(event.getHook().getLocation() != player.getLocation()) {
-                player.playSound(player.getLocation(), Sound.ENTITY_DRAGON_FIREBALL_EXPLODE, 1.0F, 1.0F);
-            }
+         ItemMeta meta = player.getInventory().getItemInMainHand().getItemMeta();
+
+        if (event.getState() != PlayerFishEvent.State.FAILED_ATTEMPT
+                && event.getState() != PlayerFishEvent.State.IN_GROUND
+                && event.getState() != PlayerFishEvent.State.REEL_IN) return;
+
+        if (meta != null && meta.getPersistentDataContainer().has(PluginKeys.GRAPPLING_HOOK.getKey())) {
+            if(!plugin.getCooldownManager().tryCooldown(player.getUniqueId(), CooldownType.GRAPPLING_HOOK, config.getLong("movementitems.grappling_hook.cooldown"))) return;
+
+            Location hookLocation = event.getHook().getLocation();
+            Location playerLocation = player.getLocation();
+
+            double distance = hookLocation.distance(playerLocation);
+            Vector direction = hookLocation.toVector().subtract(playerLocation.toVector()).normalize();
+
+            Vector velocity = direction.multiply(distance / 3.5);
+            player.setVelocity(velocity);
         }
     }
+
 }
