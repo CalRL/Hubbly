c801d6fefa856639e718e3be7ab97ab082b2391b
Cal <43823915+CalRL@users.noreply.github.com>
2024-09-10 00:37:35 +0200

    Removed unused XP listener
    (OLD) Fixed #18

--- src/main/java/me/calrl/hubbly/listeners/player/XPListener.java
+++ src/main/java/me/calrl/hubbly/listeners/player/XPListener.java
@@ -20,28 +20,14 @@ public class XPListener implements Listener {
 
         player.setLevel((int) plugin.getConfig().getDouble("player.experience.level"));
         player.setExp(1);
-        plugin.getLogger().info("set to x");
+
     }
 
     @EventHandler
     private void onPlayerXP(PlayerExpChangeEvent event) {
         if(plugin.getConfig().getBoolean("player.experience.enabled")) {
             event.setAmount(0);
-            plugin.getLogger().info("set to 0");
         }
 
-    }
-
-    public double getRequiredExp(double level) {
-        double e;
-
-        if (level <= 16) {
-           e = level * level + 6 * level;
-        } else if (level <= 31) {
-            e = (2.5 * level * level - 40.5 * level + 360);
-        } else {
-            e = (4.5 * level * level - 162.5 * level + 2220);
-        }
-        return e;
     }
 }
