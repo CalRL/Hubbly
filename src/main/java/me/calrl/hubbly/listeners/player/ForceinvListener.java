26c78b4cc4872c35be018852e0fd58b88755ce03
Cal <callumburnsoregan@gmail.com>
2024-10-01 16:24:47 +0100

    Created forceinventory

---
+++ src/main/java/me/calrl/hubbly/listeners/player/ForceinvListener.java
@@ -0,0 +1,30 @@
+package me.calrl.hubbly.listeners.player;
+
+import me.calrl.hubbly.Hubbly;
+import me.calrl.hubbly.enums.Permissions;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class ForceinvListener implements Listener {
+
+    private Hubbly plugin;
+    public ForceinvListener(Hubbly plugin) {
+        this.plugin = plugin;
+    }
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        boolean forceInventory = plugin.getConfig().getBoolean("player.forceinventory");
+
+        Player player = (Player) event.getWhoClicked();
+        if (forceInventory && !player.hasPermission(Permissions.BYPASS_FORCE_INVENTORY.getPermission())) {
+            event.setCancelled(true);
+            player.setItemOnCursor(new ItemStack(Material.AIR));
+        }
+    }
+
+}
