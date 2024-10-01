f2ed9a53ae68e38b31368092f892891a7c8c7451
Cal <callumburnsoregan@gmail.com>
2024-10-01 16:24:17 +0100

    Fixed "bypass.forceinventory" being marked as "bypass.invenrory" (lol)

--- src/main/java/me/calrl/hubbly/enums/Permissions.java
+++ src/main/java/me/calrl/hubbly/enums/Permissions.java
@@ -30,7 +30,7 @@ public enum Permissions {
     COMMAND_ADMIN("command.admin"),
     COMMAND_SELECTOR("command.selector"),
 
-    BYPASS_FORCE_INVENTORY("bypass.forceinvenory"),
+    BYPASS_FORCE_INVENTORY("bypass.forceinventory"),
     BYPASS_ANTI_SWEAR("bypass.antiswear"),
     BYPASS_BLOCKED_COMMANDS("bypass.blockedcommands"),
     BYPASS_ANTI_WDL("bypass.antiwdl"),
@@ -69,6 +69,7 @@ public enum Permissions {
     public final String getPermission() {
         return "hubbly." + this.permission;
     }
+    public final String get() { return "hubbly." + this.permission; }
 }
 
 
