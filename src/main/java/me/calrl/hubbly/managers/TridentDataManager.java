d5cae3900ed4a4b2c08969e689ae027b7d91e2bf
Cal <callumburnsoregan@gmail.com>
2025-04-27 10:06:14 +0100

    Impl TridentDataManager

--- src/main/java/me/calrl/hubbly/managers/TridentDataManager.java
+++ src/main/java/me/calrl/hubbly/managers/TridentDataManager.java
@@ -1,4 +1,32 @@
 package me.calrl.hubbly.managers;
 
+import me.calrl.hubbly.items.PlayerTridentData;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Trident;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+
 public class TridentDataManager {
+    private HashMap<Player, PlayerTridentData> playerTridents;
+    public TridentDataManager() {
+        this.playerTridents = new HashMap<>();
+    }
+
+    public void add(Player player, ItemStack trident, int slot) {
+        this.playerTridents.put(player, new PlayerTridentData(trident, slot));
+    }
+
+    public void remove(Player player) {
+        this.playerTridents.remove(player);
+    }
+
+    public void purge() {
+        this.playerTridents.clear();
+    }
+
+    public void panic() {
+
+    }
+
 }
