5098b9e8959cfa570be8271a45231c99225081c5
Cal <callumburnsoregan@gmail.com>
2025-10-08 21:31:22 +0200

    set player manually in message action

--- src/main/java/me/calrl/hubbly/action/actions/MessageAction.java
+++ src/main/java/me/calrl/hubbly/action/actions/MessageAction.java
@@ -14,7 +14,8 @@ public class MessageAction implements Action {
 
     @Override
     public void execute(Hubbly plugin, Player player, String data) {
-        new MessageBuilder(plugin, player)
+        new MessageBuilder(plugin)
+                .setPlayer(player)
                 .setMessage(data)
                 .send();
     }
