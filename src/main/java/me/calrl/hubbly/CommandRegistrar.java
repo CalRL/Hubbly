1d46d206f40c83659a6d087b8137913ec84b041d
Cal <callumburnsoregan@gmail.com>
2026-04-07 18:02:11 +0200

    commandregistrar

--- src/main/java/me/calrl/hubbly/CommandRegistrar.java
+++ src/main/java/me/calrl/hubbly/CommandRegistrar.java
@@ -1,9 +1,14 @@
 package me.calrl.hubbly;
 
 import me.calrl.hubbly.commands.*;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandMap;
 import org.bukkit.command.PluginCommand;
+
+import java.lang.reflect.Field;
+import java.util.Map;
 
 public class CommandRegistrar {
     private final Hubbly plugin;
@@ -38,7 +43,12 @@ public class CommandRegistrar {
         PluginCommand command = plugin.getCommand(commandName);
 
         boolean enabled = this.plugin.getConfig().getBoolean(enabledPath);
-        if(!enabled) return;
+
+        if(!enabled) {
+            this.unregisterCommand(commandName);
+            return;
+        }
+
         if (command == null) {
             plugin.getLogger().severe(
                     String.format("Command '%s' does not exist in plugin.yml", commandName)
@@ -47,5 +57,25 @@ public class CommandRegistrar {
         }
 
         command.setExecutor(commandClass);
+    }
+
+    private void unregisterCommand(String name) {
+        try {
+            Field commandMapField = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+            commandMapField.setAccessible(true);
+
+            CommandMap commandMap = (CommandMap) commandMapField.get(Bukkit.getServer());
+
+            Field knownCommandsField = commandMap.getClass().getDeclaredFields("knownCommands");
+            knownCommandsField.setAccessible(true);
+
+            Map<String, Command> knownCommands = (Map<String, Command>) knownCommandsField.get(commandMap);
+
+            knownCommands.remove(name);
+            knownCommands.remove("hubbly:" + name);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
