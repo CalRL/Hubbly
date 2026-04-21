b4d21cfffa1d0304b15dae0c324710d891796b32
Cal <callumburnsoregan@gmail.com>
2026-04-21 21:36:45 +0200

    add velocityaction
    todo

---
+++ src/main/java/me/calrl/hubbly/action/actions/VelocityAction.java
@@ -0,0 +1,18 @@
+package me.calrl.hubbly.action.actions;
+
+import me.calrl.hubbly.Hubbly;
+import me.calrl.hubbly.action.Action;
+import org.bukkit.entity.Player;
+
+public class VelocityAction implements Action {
+
+    @Override
+    public String getIdentifier() {
+        return "VELOCITY";
+    }
+
+    @Override
+    public void execute(Hubbly plugin, Player player, String data) {
+        // todo
+    }
+}
