b10b91b0205bfadefed99d0cdf9e8c73814b2a56
Cal <callumburnsoregan@gmail.com>
2026-07-06 19:16:54 +0200

    fix: trident / enderbow not firing
    basically itemsmanager reregistered the listeners on reload, they werent properly unregistered..

--- src/main/java/me/calrl/hubbly/ListenerRegistrar.java
+++ src/main/java/me/calrl/hubbly/ListenerRegistrar.java
@@ -40,6 +40,11 @@ public class ListenerRegistrar {
         registerListener(new InventoryListener(plugin));
         registerListener(new XPListener(plugin), "player.experience.enabled");
         registerListener(new PlayerMoveListener(plugin));
+        registerListener(new EnderbowListener(plugin));
+        registerListener(new AoteListener(plugin));
+        registerListener(new RodListener(plugin));
+        registerListener(new TridentListener(plugin));
+
 
         registerListener(new WorldEventListeners(plugin));
         registerListener(new ConfigItemListener(plugin));
