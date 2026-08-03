7a2d58bfeb044ef885162633f39bbeb984d49b5a
Cal <callumburnsoregan@gmail.com>
2026-08-03 08:54:09 +0200

    fix: readdallbossbar no longer fails if 1 player is in disabled world

--- src/main/java/me/calrl/hubbly/managers/BossBarManager.java
+++ src/main/java/me/calrl/hubbly/managers/BossBarManager.java
@@ -117,9 +117,9 @@ public class BossBarManager implements ILifecycle {
         DisabledWorlds disabledWorlds = plugin.services().disabledWorlds();
         FileConfiguration config = plugin.getConfig();
         boolean isEnaled = config.getBoolean("player.bossbar.enabled");
+        if(!isEnaled) return;
         for (Player player : Bukkit.getOnlinePlayers()) {
-            if(disabledWorlds.inDisabledWorld(player.getWorld())) return;
-            if(!isEnaled) return;
+            if(disabledWorlds.inDisabledWorld(player.getWorld())) continue;
             this.createBossBar(player);
         }
     }
